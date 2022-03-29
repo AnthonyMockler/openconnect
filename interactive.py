25054a58ef3c55de82affc2decebfdfc70c8ab8f
Anthony Mockler <git@iamcarbonatedmilk.com>
2022-03-29 16:11:58 +1100

    Removed default region if city

--- interactive.py
+++ interactive.py
@@ -76,6 +76,11 @@ def filter_elements(result):
     filtered = [element for element in elements if element is not None]
     return filtered
 
+
+@st.cache
+def download_data(viz):
+    return viz.to_csv(index=False)
+
 def unicef_blue(text,size='h3'):
     outstring = f"""<{size} style="color:#1cabe2;">{text}</h1>"""
     return outstring
@@ -100,101 +105,93 @@ with st.expander("Choose region"):
             area_name = st.selectbox("",countries,index=19)
             map_zoom = 5
         if region =='Custom Region':
-            area_name = st.text_input("Enter a City, Country","Bangkok, Thailand",placeholder="City, Country")
+            area_name = st.text_input("Enter a City, Country","",placeholder="City, Country (e.g. Bangkok, Thailand)")
             map_zoom = 9
         if region=='Custom CSV (Coming Soon)':
             area_name = st.selectbox("",countries,index=19)
 
 
 
+if len(area_name) > 3:
+    result = get_overpass_query(area_name)
+    out = merge_with_connectivity(result)
+    out['Avg Download(Mbps)'] = round((out.avg_d_kbps / 1000), 1)
+    out['Avg Upload(Mbps)'] = round((out.avg_u_kbps / 1000), 1)
+    out['Total Devices'] = round((out.devices * 4), 0)
+    total = len(out)
+    has_connectivity = len(out[pd.notnull(out.avg_d_kbps)])
+    no_connectivity = len(out[pd.isnull(out.avg_d_kbps)])
+    pct_below_10mbps = len(out[out.avg_d_kbps < 10000]) / len(out)
+    average_speed = out['Avg Download(Mbps)'].mean()
 
-result = get_overpass_query(area_name)
-out = merge_with_connectivity(result)
-out['Avg Download(Mbps)'] = round((out.avg_d_kbps / 1000), 1)
-out['Avg Upload(Mbps)'] = round((out.avg_u_kbps / 1000), 1)
-out['Total Devices'] = round((out.devices * 4), 0)
-total = len(out)
-has_connectivity = len(out[pd.notnull(out.avg_d_kbps)])
-no_connectivity = len(out[pd.isnull(out.avg_d_kbps)])
-pct_below_10mbps = len(out[out.avg_d_kbps < 10000]) / len(out)
-average_speed = out['Avg Download(Mbps)'].mean()
+    st.markdown(unicef_blue(area_name), unsafe_allow_html=True)
+    metrics = list()
+    metrics.append(["Total Schools", total])
+    metrics.append(["With Connectivity", f'{has_connectivity / total:.0%}'])
+    metrics.append(["Without Connectivity", f'{no_connectivity / total:.0%}'])
+    metrics.append(["Below 10Mbps", f'{pct_below_10mbps:.0%}'])
+    metrics.append(["Average speed (Mbps)", f"{average_speed:.0f}"])
 
-
-@st.cache
-def download_data(viz):
-    return viz.to_csv(index=False)
-#st.markdown(f"**{area_name}** has around **{total}** schools in the OpenStreetMap database")
-#st.markdown(f"Of those, **{has_connectivity / total:.0%}** have connectivity scores, **{no_connectivity / total:.0%}** had no connectivity in the Ookla dataset")
-#st.markdown(f"Average speeds are around **{average_speed:.1f}Mbps**, with {pct_below_10mbps:.0%} of schools reporting speeds below 10Mbps")
-st.markdown(unicef_blue(area_name), unsafe_allow_html=True)
-metrics = list()
-metrics.append(["Total Schools", total])
-metrics.append(["With Connectivity", f'{has_connectivity / total:.0%}'])
-metrics.append(["Without Connectivity", f'{no_connectivity / total:.0%}'])
-metrics.append(["Below 10Mbps", f'{pct_below_10mbps:.0%}'])
-metrics.append(["Average speed (Mbps)", f"{average_speed:.0f}"])
-
-metric_cols = st.columns(len(metrics))
-i = 0
-for col in metric_cols:
-    col.metric(metrics[i][0], metrics[i][1])
-    i += 1
+    metric_cols = st.columns(len(metrics))
+    i = 0
+    for col in metric_cols:
+        col.metric(metrics[i][0], metrics[i][1])
+        i += 1
 
 
-viz = out[['amenity', 'name', 'lat', 'lon', 'Avg Download(Mbps)', 'Avg Upload(Mbps)',
-           'Total Devices']].copy()
-viz['connectivity_rank'] = viz['Avg Download(Mbps)'].rank(pct=True)
-viz_has_connectivity = viz[pd.notna(viz['Avg Download(Mbps)'])]
-average = out['Avg Download(Mbps)'].plot(kind='hist')
+    viz = out[['amenity', 'name', 'lat', 'lon', 'Avg Download(Mbps)', 'Avg Upload(Mbps)',
+            'Total Devices']].copy()
+    viz['connectivity_rank'] = viz['Avg Download(Mbps)'].rank(pct=True)
+    viz_has_connectivity = viz[pd.notna(viz['Avg Download(Mbps)'])]
+    average = out['Avg Download(Mbps)'].plot(kind='hist')
 
-st.download_button("Download CSV", download_data(viz),
-                   file_name=f'{area_name}.csv')
-map_1 = px.scatter_mapbox(viz,
-                          lat="lat", lon="lon",
-                          color="connectivity_rank",
-                          color_continuous_scale=px.colors.colorbrewer.RdYlGn,
-                          size_max=15,
-                          zoom=map_zoom,
-                          mapbox_style='carto-positron',
-                          hover_data=['name', 'Avg Download(Mbps)'],
-                          height=700)
-map_1.layout.update(showlegend=False)
-st.plotly_chart(map_1, use_container_width=True)
-
-
-with st.expander("Schools without connectivity"):
-    map_2 = px.scatter_mapbox(viz[pd.isnull(viz['Avg Download(Mbps)'])],
-                              lat="lat", lon="lon",
-                              color="connectivity_rank",
-                              color_continuous_scale=px.colors.colorbrewer.RdYlGn,
-                              size_max=15,
-                              zoom=map_zoom,
-                              mapbox_style='carto-positron',
-                              hover_data=['name'],
-                              height=700)
-    map_2.layout.update(showlegend=False)
-    st.plotly_chart(map_2, use_container_width=True)
+    st.download_button("Download CSV", download_data(viz),
+                    file_name=f'{area_name}.csv')
+    map_1 = px.scatter_mapbox(viz,
+                            lat="lat", lon="lon",
+                            color="connectivity_rank",
+                            color_continuous_scale=px.colors.colorbrewer.RdYlGn,
+                            size_max=15,
+                            zoom=map_zoom,
+                            mapbox_style='carto-positron',
+                            hover_data=['name', 'Avg Download(Mbps)'],
+                            height=700)
+    map_1.layout.update(showlegend=False)
+    st.plotly_chart(map_1, use_container_width=True)
 
 
-with st.expander("Summary statistics"):
-    st.subheader(f"All schools in {area_name} by speed")
-    st.plotly_chart(average, use_container_width=True)
-    col1, col2 = st.columns(2)
+    with st.expander("Schools without connectivity"):
+        map_2 = px.scatter_mapbox(viz[pd.isnull(viz['Avg Download(Mbps)'])],
+                                lat="lat", lon="lon",
+                                color="connectivity_rank",
+                                color_continuous_scale=px.colors.colorbrewer.RdYlGn,
+                                size_max=15,
+                                zoom=map_zoom,
+                                mapbox_style='carto-positron',
+                                hover_data=['name'],
+                                height=700)
+        map_2.layout.update(showlegend=False)
+        st.plotly_chart(map_2, use_container_width=True)
 
-    with col1:
-        st.subheader('10 Fastest Schools')
-        top20 = viz.sort_values('Avg Download(Mbps)', ascending=False)[0:10]
-        top20 = top20.set_index('name').drop_duplicates()
-        top20_plot = make_bar(top20)
-        st.plotly_chart(top20_plot, use_container_width=True)
-    with col2:
-        st.subheader('10 slowest schools')
-        bottom20 = viz.sort_values('Avg Download(Mbps)', ascending=True)[0:10]
-        bottom20 = bottom20.set_index('name').drop_duplicates()
-        bottom20_plot = make_bar(bottom20)
-        st.plotly_chart(bottom20_plot, use_container_width=True)
+
+    with st.expander("Summary statistics"):
+        st.subheader(f"All schools in {area_name} by speed")
+        st.plotly_chart(average, use_container_width=True)
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.subheader('10 Fastest Schools')
+            top20 = viz.sort_values('Avg Download(Mbps)', ascending=False)[0:10]
+            top20 = top20.set_index('name').drop_duplicates()
+            top20_plot = make_bar(top20)
+            st.plotly_chart(top20_plot, use_container_width=True)
+        with col2:
+            st.subheader('10 slowest schools')
+            bottom20 = viz.sort_values('Avg Download(Mbps)', ascending=True)[0:10]
+            bottom20 = bottom20.set_index('name').drop_duplicates()
+            bottom20_plot = make_bar(bottom20)
+            st.plotly_chart(bottom20_plot, use_container_width=True)
 
 with st.expander("About this app"):
     with open('README.md', 'r') as f:
         st.markdown(f.read())
-#top10[['name','Average Download Speed (Mbps)']].plot()
