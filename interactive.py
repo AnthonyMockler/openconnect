72768c4c014b9ee8f6bbb6674b403409bfe3775e
Anthony Mockler <git@iamcarbonatedmilk.com>
2022-03-29 17:07:27 +1100

    Added hospitals

--- interactive.py
+++ interactive.py
@@ -46,6 +46,7 @@ def make_details(element):
         return None
 def make_quadkey(row):
     return qk.from_geo((row['lat'], row['lon']), 14)
+
 def merge_with_connectivity(result):
     filtered = filter_elements(result)
     out = pd.DataFrame(filtered)
@@ -85,11 +86,14 @@ def unicef_blue(text,size='h3'):
     outstring = f"""<{size} style="color:#1cabe2;">{text}</h1>"""
     return outstring
 @st.experimental_memo(show_spinner=False)
-def get_overpass_query(area_name):
+def get_overpass_query(area_name,facility):
+    amenities = dict()
+    amenities['Schools'] = 'school'
+    amenities['Hospitals'] = 'hospital'
     areaId = nominatim.query(area_name).areaId()
-    query = overpassQueryBuilder(area=areaId, elementType='way', selector='"amenity"="school"',
+    query = overpassQueryBuilder(area=areaId, elementType='way', selector=f'"amenity"="{amenities[facility]}"',
                                  out='body', includeCenter=True, includeGeometry=True)
-    with st.spinner(f"Retrieving schools in {area_name}, may take up to 60 seconds"):
+    with st.spinner(f"Retrieving {facility} in {area_name}, may take up to 60 seconds"):
         result = overpass.query(query,timeout=60)
     return result
 
@@ -98,9 +102,11 @@ with st.expander("Choose region"):
     boxcols = st.columns([1,2])
     map_zoom = 5
     with boxcols[0]:
-        region = st.radio("",['Country','Custom Region','Custom CSV (Coming Soon)'])
+        facility = st.radio("Facility Type:",['Schools','Hospitals'])
+        
 
     with boxcols[1]:
+        region = st.radio("Region Type",['Country','Custom Region','Custom CSV (Coming Soon)'])
         if region == 'Country':
             area_name = st.selectbox("",countries,index=19)
             map_zoom = 5
@@ -113,8 +119,9 @@ with st.expander("Choose region"):
 
 
 if len(area_name) > 3:
-    result = get_overpass_query(area_name)
-    out = merge_with_connectivity(result)
+    result = get_overpass_query(area_name,facility)
+    with st.spinner(f'Getting connectivity estimates for {area_name}'):
+        out = merge_with_connectivity(result)
     out['Avg Download(Mbps)'] = round((out.avg_d_kbps / 1000), 1)
     out['Avg Upload(Mbps)'] = round((out.avg_u_kbps / 1000), 1)
     out['Total Devices'] = round((out.devices * 4), 0)
@@ -126,7 +133,7 @@ if len(area_name) > 3:
 
     st.markdown(unicef_blue(area_name), unsafe_allow_html=True)
     metrics = list()
-    metrics.append(["Total Schools", total])
+    metrics.append([f"Total {facility}", total])
     metrics.append(["With Connectivity", f'{has_connectivity / total:.0%}'])
     metrics.append(["Without Connectivity", f'{no_connectivity / total:.0%}'])
     metrics.append(["Below 10Mbps", f'{pct_below_10mbps:.0%}'])
@@ -160,7 +167,7 @@ if len(area_name) > 3:
     st.plotly_chart(map_1, use_container_width=True)
 
 
-    with st.expander("Schools without connectivity"):
+    with st.expander(f"{facility} without connectivity"):
         map_2 = px.scatter_mapbox(viz[pd.isnull(viz['Avg Download(Mbps)'])],
                                 lat="lat", lon="lon",
                                 color="connectivity_rank",
@@ -175,18 +182,18 @@ if len(area_name) > 3:
 
 
     with st.expander("Summary statistics"):
-        st.subheader(f"All schools in {area_name} by speed")
+        st.subheader(f"All {str.lower(facility)} in {area_name} by speed")
         st.plotly_chart(average, use_container_width=True)
         col1, col2 = st.columns(2)
 
         with col1:
-            st.subheader('10 Fastest Schools')
+            st.subheader(f'10 Fastest {str.lower(facility)}')
             top20 = viz.sort_values('Avg Download(Mbps)', ascending=False)[0:10]
             top20 = top20.set_index('name').drop_duplicates()
             top20_plot = make_bar(top20)
             st.plotly_chart(top20_plot, use_container_width=True)
         with col2:
-            st.subheader('10 slowest schools')
+            st.subheader(f'10 slowest {str.lower(facility)}')
             bottom20 = viz.sort_values('Avg Download(Mbps)', ascending=True)[0:10]
             bottom20 = bottom20.set_index('name').drop_duplicates()
             bottom20_plot = make_bar(bottom20)
